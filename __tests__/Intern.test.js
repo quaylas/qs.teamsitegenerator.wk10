98c68da416531ee42c9419ddd68764de3d561b41
quaylas <quayla@cxadvisors.com>
2021-01-24 15:03:25 -0700

    added unit tests and passing functions for Intern

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
-const Employee = require('../lib/Employee');
+const Intern = require('../lib/Intern');
+
+test('creates a new intern', () => {
+    const intern = new Intern('mary', 8301797,'marys@test.com', 'The Burr, Sir');
+
+    expect(intern.school).toEqual(expect.any(String)); //all other properties are tested by Employee tests
+});
+
+test('gets an intern\'s role and returns Intern', () => {
+    const intern = new Intern('mary', 8301797,'marys@test.com', 'The Burr, Sir');
+
+    expect(intern.getRole()).toEqual('Intern');
+});
+
+test('gets an intern\'s school', () => {
+    const intern = new Intern('mary', 8301797,'marys@test.com', 'The Burr, Sir');
+
+    expect(intern.getSchool()).toEqual(intern.school);
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,13 +1,13 @@
 const Manager = require('../lib/Manager');
 
 test('creates a new manager', () => {
-    const manager = new Manager('Amelia','07241937','ameliaa@test.com', 1937);
+    const manager = new Manager('Amelia',7241937,'ameliaa@test.com', 1937);
 
     expect(manager.officeNumber).toEqual(expect.any(Number)); //all other properties are tested by Employee tests
 });
 
 test('gets a manager\'s role and returns Manager', () => {
-    const manager = new Manager('Amelia','07241937','ameliaa@test.com', 1937);
+    const manager = new Manager('Amelia', 7241937 , 'ameliaa@test.com', 1937);
 
     expect(manager.getRole()).toEqual('Manager');
-})
+});
